c0b0518c12b6195e04009cf9314fb829c35c8f7b
Leah <leah.jenkins2019@gmail.com>
2023-01-05 11:10:17 -0500

    commit tree 621efab93bd6f80b8b8b16607a17fc6d4c7fdc41

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution in this file!
+const employee = {
+name : "Leah",
+streetName : "1153 Acorn St"
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+ const newObj = {...obj}
+ newObj[key] = value
+ return newObj
+}
+
+console.log(updateEmployeeWithKeyAndValue(myObject, "favoriteColor", "Purple"))
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key] = value
+    return obj
+}
+
+console.log(destructivelyUpdateEmployeeWithKeyAndValue(employee, "name", "Sam"))
+
+function deleteFromEmployeeByKey(obj, key){
+    const newObj = {...obj}
+    newObj[key] = undefined
+    return newObj
+}
+
+console.log(deleteFromEmployeeByKey(myObject, "name"))
+
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    obj[key] = undefined
+    return obj
+}
+
+console.log(deleteFromEmployeeByKey(employee, "name"))
